a201520b5a787c692ef5d9c47ae072b685bd430c
Russell Sutter <russell.sutter@gmail.com>
2020-08-05 12:53:35 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,20 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  attr_accessor :brand
 
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
+  end
+  
+  def self.brands
+    BRANDS.uniq
   end
 
 end
